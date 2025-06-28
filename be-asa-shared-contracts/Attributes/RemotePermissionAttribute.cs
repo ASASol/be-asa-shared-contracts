5abc9709dd6dbdce815e863418a3376f87e63313
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-06-28 14:00:06 +0700

    fix(inteface): fix interface permission check + Attribute

---
+++ be-asa-shared-contracts/Attributes/RemotePermissionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using be_asa_shared_contracts.Enums;
+
+namespace be_asa_shared_contracts.Attributes
+{
+    public class RemotePermissionAttribute : Attribute
+    {
+        public FunctionCode FunctionCode { get; }
+        public CommandName CommandName { get; }
+
+        public RemotePermissionAttribute(FunctionCode functionCode, CommandName commandName)
+        {
+            FunctionCode = functionCode;
+            CommandName = commandName;
+        }
+    }
+}
