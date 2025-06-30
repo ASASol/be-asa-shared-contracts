5716af7265a4cf237121aeb651b272724f806092
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-06-30 08:43:35 +0700

    feat haspermission check

---
+++ be-asa-shared-contracts/DTO/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace be_asa_shared_contracts.DTO
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+    }
+}
---
+++ be-asa-shared-contracts/DTO/PermissionCheckResult.cs
@@ -0,0 +1,7 @@
+﻿namespace be_asa_shared_contracts.DTO
+{
+    public class PermissionCheckResult
+    {
+        public bool HasPermission { get; set; }
+    }
+}
