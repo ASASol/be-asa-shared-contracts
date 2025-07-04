ce580417cdc524c8ac71299f3d31fc41c6762a08
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2025-07-04 09:13:50 +0700

    add utils

---
+++ be-asa-shared-contracts/Utils/CoreHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace be_asa_shared_contracts.Utils
+{
+    public class CoreHelper
+    {
+        public static DateTimeOffset SystemTimeNow => TimeHelper.ConvertToUtcPlus7(DateTimeOffset.Now);
+    }
+}
---
+++ be-asa-shared-contracts/Utils/ITracking.cs
@@ -0,0 +1,15 @@
+﻿namespace be_asa_shared_contracts.Utils
+{
+    public interface ITracking
+    {
+        string CreatedBy { get; set; }
+        string LastUpdatedBy { get; set; }
+        string? DeletedBy { get; set; }
+
+        DateTimeOffset CreatedTime { get; set; }
+        DateTimeOffset LastUpdatedTime { get; set; }
+        DateTimeOffset? DeletedTime { get; set; }
+
+        bool IsDeleted { get; set; }
+    }
+}
