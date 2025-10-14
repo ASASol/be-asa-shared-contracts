e0ab0eaa947167106393715f4bf944d61096433a
Huy Phạm <huypt@namviets.com>
2025-10-14 10:03:03 +0700

    add enum theme

---
+++ be-asa-shared-contracts/Utils/ThemeBase.cs
@@ -0,0 +1,10 @@
+﻿namespace be_asa_shared_contracts.Utils
+{
+    public class ThemeBase : BaseEntity
+    {
+        public string Name { get; set; }
+        public string JsonConfig { get; set; }
+        public int? Version { get; set; }
+        public bool IsDefault { get; set; }
+    }
+}
