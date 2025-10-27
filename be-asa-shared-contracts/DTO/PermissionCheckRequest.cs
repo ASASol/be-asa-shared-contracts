29442cf502bc6a24ce881c40d8a3937ec974787c
Huy Phạm <huypt@namviets.com>
2025-10-27 15:14:27 +0700

    Update PermissionCheckRequest.cs

--- be-asa-shared-contracts/DTO/PermissionCheckRequest.cs
+++ be-asa-shared-contracts/DTO/PermissionCheckRequest.cs
@@ -5,8 +5,8 @@ namespace be_asa_shared_contracts.DTO
     public class PermissionCheckRequest
     {
         public Guid UserId { get; set; }
-        public FunctionCode FunctionCode { get; set; }
-        public CommandName CommandName { get; set; }
+        public string FunctionCode { get; set; }
+        public string CommandName { get; set; }
         public Guid BranchId { get; set; }
     }
 }
