5787b7e34134e1478efd55a7cb823214d1156f0d
Huy Phạm <huypt@namviets.com>
2025-10-31 15:12:33 +0700

    add DTO

---
+++ be-asa-shared-contracts/DTO/Messaging/RabbitMQ/EmailAttachmentQueuePayload.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace be_asa_shared_contracts.DTO.Messaging.RabbitMQ
+{
+    public class EmailAttachmentQueuePayload
+    {
+        public string FileName { get; set; }
+        public string ContentType { get; set; }
+        public string Base64Content { get; set; }
+    }
+}
---
+++ be-asa-shared-contracts/DTO/Messaging/RabbitMQ/SendEmailQueuePayload.cs
@@ -0,0 +1,14 @@
+﻿namespace be_asa_shared_contracts.DTO.Messaging.RabbitMQ
+{
+    public class SendEmailQueuePayload
+    {
+        public string ConfigKey { get; set; } = "Default";
+        public string? To { get; set; }
+        public List<string>? Cc { get; set; }
+        public List<string>? Bcc { get; set; }
+        public string Subject { get; set; } = default!;
+        public string Body { get; set; } = default!;
+        public List<EmailAttachmentQueuePayload>? Attachments { get; set; }
+
+    }
+}
