7301d2c5593c0636f4f5e4913dc12a69bcbfc79b
Huy Phạm <huypt@namviets.com>
2026-01-13 15:16:16 +0700

    fix DTO

--- be-asa-shared-contracts/DTO/PermissionCheckRequest.cs
+++ be-asa-shared-contracts/DTO/PermissionCheckRequest.cs
@@ -1,6 +1,4 @@
-﻿using be_asa_shared_contracts.Enums;
-
-namespace be_asa_shared_contracts.DTO
+﻿namespace be_asa_shared_contracts.DTO
 {
     public class PermissionCheckRequest
     {
@@ -8,5 +6,7 @@ namespace be_asa_shared_contracts.DTO
         public string FunctionCode { get; set; }
         public string CommandName { get; set; }
         public Guid BranchId { get; set; }
+        public string? DepartmentCode { get; set; }
+        public string? OutletCode { get; set; }
     }
 }
--- be-asa-shared-contracts/DTO/PermissionCheckScopeRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace be_asa_shared_contracts.DTO
-{
-    public class PermissionCheckScopeRequest : PermissionCheckRequest
-    {
-        public string? DepartmentCode { get; set; }
-        public string? OutletCode { get; set; }
-    }
-}
