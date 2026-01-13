c837b58368246b17b60df956fbf9f59e3372c7a7
Huy Phạm <huypt@namviets.com>
2026-01-13 14:00:19 +0700

    Create PermissionCheckScopeRequest.cs

---
+++ be-asa-shared-contracts/DTO/PermissionCheckScopeRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace be_asa_shared_contracts.DTO
+{
+    public class PermissionCheckScopeRequest : PermissionCheckRequest
+    {
+        public string? DepartmentCode { get; set; }
+        public string? OutletCode { get; set; }
+    }
+}
