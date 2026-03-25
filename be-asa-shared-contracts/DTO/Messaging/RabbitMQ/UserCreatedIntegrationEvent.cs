bb89d54d90650819c4917cfc8b6fd4e88341ac68
Huy Phạm <huypt@namviets.com>
2026-03-25 14:53:04 +0700

    update mq model

---
+++ be-asa-shared-contracts/DTO/Messaging/RabbitMQ/UserCreatedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace be_asa_shared_contracts.DTO.Messaging.RabbitMQ
+{
+    public class UserCreatedIntegrationEvent
+    {
+        public Guid UserId { get; set; }
+        public string? CustomerCode { get; set; }
+        public string? Name { get; set; }
+        public string? Avatar { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
