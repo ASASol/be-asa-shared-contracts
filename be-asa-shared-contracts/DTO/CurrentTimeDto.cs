7a3e158d9aa67315ff4644fb4714b36ad65477a3
Huy Phạm <huypt110402@gmail.com>
2026-04-01 10:42:23 +0700

    update sub

--- be-asa-shared-contracts/DTO/CurrentTimeDto.cs
+++ be-asa-shared-contracts/DTO/CurrentTimeDto.cs
@@ -2,10 +2,15 @@
 {
     public class CurrentTimeDto
     {
-        public DateTimeOffset SystemTimeNow { get; set; }
-        public string Year { get; set; }     // yyyy
-        public string Date { get; set; }     // yyyyMMdd
-        public string Time { get; set; }     // HH:mm
-        public string MonthDay { get; set; } // MMdd
+        public DateTimeOffset UtcTime { get; set; }
+        public string Year { get; set; }     // yyyy   => UTC
+        public string Date { get; set; }     // yyyyMMdd  => UTC
+        public string Time { get; set; }     // HH:mm  => UTC
+        public string MonthDay { get; set; } // MMdd   => UTC
+
+        public string LocalYear { get; set; }     // yyyy
+        public string LocalDate { get; set; }     // yyyyMMdd
+        public string LocalTime { get; set; }     // HH:mm
+        public string LocalMonthDay { get; set; } // MMd
     }
 }
