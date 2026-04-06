30b57b7cd21169fc16a93229a6642ed785963ebb
Huy Phạm <huypt110402@gmail.com>
2026-04-06 14:48:48 +0700

    update contract

---
+++ be-asa-shared-contracts/Constants/ModuleCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace be_asa_shared_contracts.Constants
+{
+    public static class ModuleCodes
+    {
+        public const string Authentication = "authentication";
+        public const string FoodOrder = "food-order";
+        public const string HotelBooking = "hotel-booking";
+        public const string Website = "website";
+        public const string Local = "local";
+    }
+}
