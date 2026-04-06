527c7e1225e2db093cadcdb9396fb0e4aa8a3727
Huy Phạm <huypt110402@gmail.com>
2026-04-06 15:26:46 +0700

    update sub

---
+++ be-asa-shared-contracts/Constants/TableCodes.cs
@@ -0,0 +1,7 @@
+﻿namespace be_asa_shared_contracts.Constants
+{
+    public static class TableCodes
+    {
+        public const string FB_DBH = "FB_DonBanHang";
+    }
+}
--- be-asa-shared-contracts/Enums/TableCode.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace be_asa_shared_contracts.Enums
-{
-    public enum TableCode
-    {
-        FB_DonBanHang
-    }
-}
